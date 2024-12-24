d6bfbaed2c29dfa1513e6776e547d22bf6a003db
Cauã de Lima <caua-l1@outlook.com>
2024-12-24 19:19:42 -0300

    Adicionado página de login e estilização

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 app.listen(PORT, () => {
     console.log('Servidor inicializado.');
 });

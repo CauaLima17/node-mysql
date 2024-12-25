77c6bd6b8a7527983fea547792dc5761f8a2fe7b
Cauã de Lima <caua-l1@outlook.com>
2024-12-25 18:47:04 -0300

    Adicionado consulta no database para autenticação de registro + alteração no estilo das mensagens de erro na página de registro

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,6 @@
 import dbConnection from "../config/DBconnect.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 const AuthController = {
     register(req, res){
@@ -6,23 +8,37 @@ const AuthController = {
 
         const { name, email, password, passwordRetry } = req.body;
 
-        dbConnection.query('SELECT email FROM users WHERE email = ?', [email], (err, result) => {
+        dbConnection.query('SELECT email FROM users WHERE email = ?', [email], async (err, result) => {
             if (err) {
                 console.log(err);
             }
 
             if (result.length > 0) {
                 return res.render('register', {
-                    message: 'That email is already in use'
+                    message: 'That email is already in use',
+                    err: true
                 });
             } else if (password !== passwordRetry) {
                 return res.render('register', {
-                    message: 'Passwords do not match'
+                    message: 'Passwords do not match',
+                    err: true
                 });
             }
-        });
 
-        res.send('Form submited');
+            let hashedPassword = await bcrypt.hash(password, 8);
+            console.log(hashedPassword);
+
+            dbConnection.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (err, results) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    return res.render('register', {
+                        message: 'User registered successfully',
+                        err: false
+                    })
+                }
+            });
+        });
     }
 };
 
